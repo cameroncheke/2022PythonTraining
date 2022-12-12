84c7114546b66bed422297332937c560f18edcef
System Administrator <root@iMac.local>
2022-12-12 12:11:43 -0500

    Adding RithmCoursePart1.py

---
+++ RithmCoursePart1.py
@@ -0,0 +1,31 @@
+#Boolean logic basic exercises Rithm School
+
+first = "Hello World"
+
+print("I AM A COMPUTER!")
+
+if 1 < 2 and 4 > 2:
+    print("math is Fun")
+
+nope = None
+
+print(nope)
+
+maybe = True and False
+
+print(maybe)
+
+numberofletters = len("What's my length?")
+print(numberofletters)
+shout = str.upper("i am shouting")
+print(shout)
+
+print(type([]))
+
+print("apple".find("l"))
+
+print("y" in "xylophone")
+
+userprompt = 'i love to see'
+
+print(userprompt.islower())
